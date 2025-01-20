3812e0cee1a623ffba9ce2f6d1d33c22cfb213a6
john.cumby <john.cumby.jc@gmail.com>
2025-01-20 14:41:04 -0330

    Added Comments to all Classes

--- src/main/java/MovieRatingJob.java
+++ src/main/java/MovieRatingJob.java
@@ -1,23 +1,24 @@
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration; // To configure the job.
+import org.apache.hadoop.fs.Path; // To handle file paths in HDFS.
+import org.apache.hadoop.io.IntWritable; // Hadoop data type that represents an integer value - specifically designed for use in MapReduce prgorams.
+import org.apache.hadoop.mapreduce.Job; // To define and run MapReduce jobs.
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; // To read data from HDFS.
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; // To write data from HDFS.
 import org.apache.hadoop.mapreduce.Job;
 
 public class MovieRatingJob {
+    // This is where the MapReduce job is defined and executed.
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Movie Rating Counter");
-        job.setJarByClass(MovieRatingJob.class);
-        job.setMapperClass(RatingMapper.class);
-        job.setCombinerClass(RatingReducer.class);
-        job.setReducerClass(RatingReducer.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        Configuration conf = new Configuration(); // Create configuration object, used to store configuration settings for the job.
+        Job job = Job.getInstance(conf, "Movie Rating Counter"); // Create new Job instance.
+        job.setJarByClass(MovieRatingJob.class); // Set the JAR file containing the job code. This tells Hadoop where to find the classes and methods for the job.
+        job.setMapperClass(RatingMapper.class); // This tells Hadoop to use the RatingMapper class as a Mapper for this job.
+        job.setCombinerClass(RatingReducer.class); // This tells Hadoop to use the RatingReducer class as a combiner.
+        job.setReducerClass(RatingReducer.class); // This tells Hadoop to use the RatingReducer class as the Reducer for this job.
+        job.setOutputKeyClass(IntWritable.class); // This tells Hadoop that the output keys of the MapReduce job will be of type IntWritable.
+        job.setOutputValueClass(IntWritable.class); // This tells Hadoop that the output values of the MapReduce job will be of type IntWritable.
+        FileInputFormat.addInputPath(job, new Path(args[0])); // Specifies the input path for the MapReduce job. args[0] refers to the first command-line argument that you provide when running the job.
+        FileOutputFormat.setOutputPath(job, new Path(args[1])); // Specifies the output path for the MapReduce job. args[1] refers to the second command-line argument that you provide when running the job.
+        System.exit(job.waitForCompletion(true) ? 0 : 1); // Run the MapReduce job and wait for it to complete.
     }
 }
--- src/main/java/RatingReducer.java
+++ src/main/java/RatingReducer.java
@@ -1,26 +1,34 @@
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.IntWritable; // Hadoop data type that represents an integer value - specifically designed for use in MapReduce programs.
+import org.apache.hadoop.mapreduce.Reducer; // A Hadoop MapReduce package. Provides the foundation for writing Reducer functions in MapReduce programs.
 
 import java.io.IOException;
 
-
+// Define the RatingReducer class that extends the Reducer class.
 public class RatingReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
 
+    // Create new IntWritable object to store the calculated sum of ratings.
     private final IntWritable result = new IntWritable();
 
+    // Override the map method of the parent class Mapper.
     @Override
+
+    // This method is called once for each unique key emitted by the Mapper. In this instance, 5 times.
+    // The first time it's called, it will iterate over every key-value pair with a key of 1,
+    // add `1` to the sum each time, and emit the result to the next stage of the MapReduce job.
+
     public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
         throws IOException, InterruptedException {
         try {
-            int sum = 0;
+            int sum = 0; //Initialize sum variable to keep track of the total count for the current rating.
+            // Iterate over all values.
             for (IntWritable val : values) {
+                // Add each value to the sum for that value.
                 sum += val.get();
             }
-            result.set(sum);
-            context.write(key, result);
+            result.set(sum); // Set the result variable to the calculated sum.
+            context.write(key, result); // Emit the key (the rating) and the result (the total count) to the next stage of the job.
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
